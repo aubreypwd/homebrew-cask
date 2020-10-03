f876bc806f24509d2bf498b883a62f28a0cffde6
Aubrey Portwood <code@aubreypwd.com>
2020-10-03 13:49:09 -0600

    Add Reedsy for Mac cask.

---
+++ Casks/reedsy-mac.rb
@@ -0,0 +1,14 @@
+cask "reedsy-mac" do
+  version "1.0.0"
+  sha256 :no_check
+
+  url "https://github.com/aubreypwd/reedsy-mac/releases/download/#{version}/Reedsy.#{version}.dmg"
+  appcast "https://github.com/aubreypwd/reedsy-mac/releases/download/"
+  name "Reedsy for Mac"
+  homepage "https://github.com/aubreypwd/reedsy-mac/"
+
+  app "Reedsy.app"
+
+  zap trash: [
+  ]
+end
--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-# Aubreypwd Tap
+# Tap
+
+My person tap for my `brew cask` apps.
 
 ## How do I install these formulae?
+
 `brew install aubreypwd/tap/<formula>`
 
 Or `brew tap aubreypwd/tap` and then `brew install <formula>`.
 
 ## Documentation
+
 `brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
