d2047d14fb4b11b6e87316b0fc4596bfde5357fa
Aubrey Portwood <code@aubreypwd.com>
2020-11-02 11:30:51 -0700

    There is now a v from npm version.

--- Casks/google-voice.rb
+++ Casks/google-voice.rb
@@ -2,7 +2,7 @@ cask "google-voice" do
   version "1.0.1"
   sha256 :no_check
 
-  url "https://github.com/aubreypwd/google-voice-mac/releases/download/#{version}/Google.Voice.#{version}.dmg"
+  url "https://github.com/aubreypwd/google-voice-mac/releases/download/v#{version}/Google.Voice.#{version}.dmg"
   appcast "https://github.com/aubreypwd/google-voice-mac/releases/download/"
   name "Google Voice for Mac"
   homepage "https://github.com/aubreypwd/google-voice-mac/"
