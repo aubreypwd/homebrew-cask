c3c0a2937a00b568424a48ba771121528ecc3e8e
Aubrey Portwood <code@aubreypwd.com>
2020-11-02 11:17:05 -0700

    Bump the latest version.

--- Casks/google-voice.rb
+++ Casks/google-voice.rb
@@ -1,5 +1,5 @@
 cask "google-voice" do
-  version "1.0.0"
+  version "1.0.1"
   sha256 :no_check
 
   url "https://github.com/aubreypwd/google-voice-mac/releases/download/#{version}/Google.Voice.#{version}.dmg"
