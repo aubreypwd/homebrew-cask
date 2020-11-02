cc4280faa38a01562c2434ca7dc84cf86407c201
Aubrey Portwood <code@aubreypwd.com>
2020-11-02 15:28:44 -0700

    Add youtube.

---
+++ Casks/youtube.rb
@@ -0,0 +1,13 @@
+cask "youtube" do
+  version "1.0.1"
+  sha256 :no_check
+
+  url "https://github.com/aubreypwd/youtube-mac/releases/download/v1.0.0/Youtube.1.0.0.dmg"
+  appcast "https://github.com/aubreypwd/youtube-mac/releases/download/"
+  name "Youtube for Mac"
+  homepage "https://github.com/aubreypwd/youtube-mac/"
+
+  app "Google Voice.app"
+
+  zap trash: []
+end
