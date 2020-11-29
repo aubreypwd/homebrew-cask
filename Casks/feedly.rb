393be5dc41778fcf1936fdefed068b8c386a4155
Aubrey Portwood <code@aubreypwd.com>
2020-11-28 17:05:38 -0700

    Align these a bit more.

--- Casks/feedly.rb
+++ Casks/feedly.rb
@@ -1,12 +1,12 @@
 cask "feedly" do
-  sha256 :no_check
-
-  url "https://github.com/aubreypwd/feedly-mac/releases/download/1.0.0/feedly.1.0.0.dmg"
-  appcast "https://github.com/aubreypwd/feedly-mac"
   name "Feedly"
-  homepage "https://github.com/aubreypwd/feedly-mac/"
-
   app "feedly.app"
 
+  url "https://github.com/aubreypwd/feedly-mac/releases/download/1.0.0/feedly.1.0.0.dmg"
+
+  appcast "https://github.com/aubreypwd/feedly-mac"
+  homepage "https://github.com/aubreypwd/feedly-mac/"
+
   zap trash: []
+  sha256 :no_check
 end
