49a926c8a93c1c9782e82ebb7546cafb494e4189
Aubrey Portwood <code@aubreypwd.com>
2020-11-28 17:07:02 -0700

    Remove versions from these.

--- Casks/chillhop-radio.rb
+++ Casks/chillhop-radio.rb
@@ -1,13 +1,12 @@
 cask "chillhop-radio" do
-  version "1.0.0"
-  sha256 :no_check
-
-  url "https://github.com/aubreypwd/chillhop-radio-mac/releases/download/1.0.1/Chillhop.Radio.1.0.1.dmg"
-  appcast "https://github.com/aubreypwd/chillhop-radio-mac"
   name "Chillhop Radio for Mac"
-  homepage "https://github.com/aubreypwd/chillhop-radio-mac/"
-
   app "Chillhop Radio.app"
 
+  url "https://github.com/aubreypwd/chillhop-radio-mac/releases/download/1.0.1/Chillhop.Radio.1.0.1.dmg"
+
+  appcast "https://github.com/aubreypwd/chillhop-radio-mac"
+  homepage "https://github.com/aubreypwd/chillhop-radio-mac/"
+
   zap trash: []
+  sha256 :no_check
 end
