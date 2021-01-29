03556e03d0965e143b4203f1e2a701e9d0d0d39d
Aubrey Portwood <aubreypwd@users.noreply.github.com>
2021-01-29 08:34:53 -0700

    Update google-voice to v1.1.1

--- Casks/google-voice.rb
+++ Casks/google-voice.rb
@@ -1,13 +1,11 @@
 cask "google-voice" do
-  version "1.0.1"
-  sha256 :no_check
-
-  url "https://github.com/aubreypwd/google-voice-mac/releases/download/v#{version}/Google.Voice.#{version}.dmg"
-  appcast "https://github.com/aubreypwd/google-voice-mac/releases/download/"
   name "Google Voice for Mac"
-  homepage "https://github.com/aubreypwd/google-voice-mac/"
-
   app "Google Voice.app"
 
+  url "https://github.com/aubreypwd/google-voice-mac/releases/download/v1.1.1/Google.Voice.1.1.1.dmg"
+  appcast "https://github.com/aubreypwd/google-voice-mac/releases/download/"
+  homepage "https://github.com/aubreypwd/google-voice-mac/"
+
+  sha256 :no_check
   zap trash: []
 end
