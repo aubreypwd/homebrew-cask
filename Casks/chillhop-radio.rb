c98fc7d08f90f4edab3d23f1177692def47638f5
Aubrey Portwood <code@aubreypwd.com>
2021-02-26 11:40:01 -0700

    Update to 2.0.0

--- Casks/chillhop-radio.rb
+++ Casks/chillhop-radio.rb
@@ -2,7 +2,7 @@ cask "chillhop-radio" do
   name "Chillhop Radio for Mac"
   app "Chillhop Radio.app"
 
-  url "https://github.com/aubreypwd/chillhop-radio-mac/releases/download/1.0.1/Chillhop.Radio.1.0.1.dmg"
+  url "https://github.com/aubreypwd/chillhop-radio-mac/releases/download/v2.0.0/Chillhop.Radio.2.0.0.dmg"
 
   appcast "https://github.com/aubreypwd/chillhop-radio-mac"
   homepage "https://github.com/aubreypwd/chillhop-radio-mac/"
