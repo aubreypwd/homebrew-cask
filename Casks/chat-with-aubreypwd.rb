d3cb61459193e30afbc57d46e03613e78269f4a5
Aubrey Portwood <code@aubreypwd.com>
2021-07-25 18:14:16 -0600

    this is the order I want to use.

--- Casks/chat-with-aubreypwd.rb
+++ Casks/chat-with-aubreypwd.rb
@@ -1,15 +1,15 @@
 cask "chat-with-aubreypwd" do
   sha256 :no_check
 
+  appcast "https://github.com/aubreypwd/chat-with-aubreypwd-mac/releases/download/"
+  homepage "https://github.com/aubreypwd/chat-with-aubreypwd-mac/"
+
   name "Chat with @aubreypwd"
 
   version "1.0.1" # Note, no v
   url "https://github.com/aubreypwd/chat-with-aubreypwd-mac/releases/download/v#{version}/Chat.with.@aubreypwd.#{version}.dmg"
-
-  appcast "https://github.com/aubreypwd/chat-with-aubreypwd-mac/releases/download/"
-  homepage "https://github.com/aubreypwd/chat-with-aubreypwd-mac/"
-
   app "Chat with @aubreypwd.app"
+
 
   zap trash: []
 end
